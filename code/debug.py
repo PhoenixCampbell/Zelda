b62ac82b0dd74afe234795bfcce5f501e0e7a411
Phoenix Campbell <Phoenix.Campbell@trojans.dsu.edu>
2023-12-29 16:30:55 -0600

    Fixed spelling mistakes and stylized code

--- code/debug.py
+++ code/debug.py
@@ -3,8 +3,8 @@ pygame.init()
 font = pygame.font.Font(None,30)
 
 def debug(info,y = 10, x = 10):
-	display_surface = pygame.display.get_surface()
-	debug_surf = font.render(str(info),True,'White')
-	debug_rect = debug_surf.get_rect(topleft = (x,y))
-	pygame.draw.rect(display_surface,'Black',debug_rect)
-	display_surface.blit(debug_surf,debug_rect)
+	displaySurface = pygame.display.get_surface()
+	debugSurf = font.render(str(info),True,'White')
+	debugRect = debugSurf.get_rect(topleft = (x,y))
+	pygame.draw.rect(displaySurface,'Black',debugRect)
+	displaySurface.blit(debugSurf,debugRect)
--- code/level.py
+++ code/level.py
@@ -8,25 +8,25 @@ class Level:
 	def __init__(self):
 
 		# get the display surface 
-		self.display_surface = pygame.display.get_surface()
+		self.displaySurface = pygame.display.get_surface()
 
 		# sprite group setup
-		self.visible_sprites = pygame.sprite.Group()
-		self.obstacle_sprites = pygame.sprite.Group()
+		self.visibleSprites = pygame.sprite.Group()
+		self.obstacleSprites = pygame.sprite.Group()
 
 		# sprite setup
 		self.create_map()
 
 	def create_map(self):
-		for row_index,row in enumerate(WORLD_MAP):
-			for col_index, col in enumerate(row):
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
+		for rowIndex,row in enumerate(worldMap):
+			for colIndex, col in enumerate(row):
+				x = colIndex * TILESIZE
+				y = rowIndex * TILESIZE
 				if col == 'x':
-					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+					Tile((x,y),[self.visibleSprites,self.obstacleSprites])
 				if col == 'p':
-					Player((x,y),[self.visible_sprites])
+					Player((x,y),[self.visibleSprites])
 
 	def run(self):
 		# update and draw the game
-		self.visible_sprites.draw(self.display_surface)
+		self.visibleSprites.draw(self.displaySurface)
